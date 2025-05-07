ffbc259c6c9d7d63a3b86f25032f8e4bb2d1ffb4
srah17898 <litmate7@gmail.com>
2025-05-07 16:23:09 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-});
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
